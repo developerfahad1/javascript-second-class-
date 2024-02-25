b5a9d41058d30a22d9f42f9a2ccf2d4713195368
developerfahad1 <fahadgraphic.pk>
2024-02-24 23:15:51 -0800

    index.html and app.js file created

---
+++ App.js
@@ -0,0 +1,59 @@
+// alert("Hello Muhammad Fahad");
+
+// var num1 = 10;
+// var num2 = '20';
+// var num3 = 20;
+
+// console.log(typeof(num2)) 
+// console.log(num1 + num2 + num3) 
+
+// var username = prompt("enter your username");
+
+// console.log(username)
+
+// var num1 = Number(prompt('Enter Your First number'));
+// var num2 = Number(prompt('Enter Your Second Number'));
+
+// var num1 = +prompt('Enter Your First number'));
+// var num2 = +prompt('Enter Your Second Number'));
+// console.log(num1 + num2)
+
+
+// Task one started 
+
+// var num1 = +prompt('Enter Your First Number');
+// var num2 = +prompt('Enter Your Second Number');
+// var num3 = +prompt('Enter Your Third Number');
+// var num4 = +prompt('Enter Your Fourth Number');
+
+// console.log(num1 + num2 + num3 + num4);
+
+
+// four types of javascript 
+
+// Arithematics Operator 
+// +
+// -
+// *
+// /
+// %
+
+
+var num1 = 10;
+var num2 = 20;
+
+// // console.log(num1 + num2)
+// // console.log(num1 - num2)
+// // console.log(num1 * num2)
+// // console.log(num1 / num2)
+// console.log(' muhammadabdullah ===> ' ,num1 % num2)
+// console.log(10 + 20 - 10 * 5 / 2);
+
+// Assignment Operator started 
+// =
+// +=
+// -=
+// /=
+// *=
+
+
